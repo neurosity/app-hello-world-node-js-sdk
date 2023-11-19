e13f563ffbed24aca9da3b18e181fcff74ffe8d9
AJ Keller <aj@neurosity.co>
2023-11-19 14:21:43 -0800

    FIX: deps

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Notion } = require("@neurosity/notion");
+const { Neurosity } = require("@neurosity/sdk");
 require('dotenv').config();
 
 const deviceId = process.env.DEVICE_ID || "";
@@ -17,12 +17,12 @@ const verifyEnvs = (email, password, deviceId) => {
 verifyEnvs(email, password, deviceId);
 console.log(`${email} attempting to authenticate with ${deviceId}`);
 
-const notion = new Notion({
+const neurosity = new Neurosity({
   deviceId
 });
 
 const main = async () => {
-  await notion.login({
+  await neurosity.login({
     email,
     password
   })
@@ -32,7 +32,7 @@ const main = async () => {
   });
   console.log("Logged in");
 
-  notion.calm().subscribe((calm) => {
+  neurosity.calm().subscribe((calm) => {
     if (calm.probability > 0.3) {
       console.log("Hello world");
     }
